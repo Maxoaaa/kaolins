320cfa045b2b07a701d9e45b4fe0a3b50312c28e
Theofilus <realth99@gmail.com>
2018-12-06 21:47:36 +0700

    update

--- app/src/main/java/id/ac/upj/tif/menghitungluas/Main6aActivity.java
+++ app/src/main/java/id/ac/upj/tif/menghitungluas/Main6aActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Main6aActivity extends AppCompatActivity {
-    Button btnHitung,btnHitung3, btnNext;
+    Button btnHitung,btnHitung3, btnNext, btnHasil;
     EditText txtX1,txtX2,txtX3,txtX4,txtX5,txtX6,txtX7,txtX8,txtX9,txtX10,txtXi;
     EditText txtYtr,txtSn, txtYn;
     TextView hasilAveX,hasilAveX1, hasilKtr,hasilSd,hasilXtr;
@@ -24,6 +24,7 @@ public class Main6aActivity extends AppCompatActivity {
 
         btnNext = findViewById(R.id.btnNext);
         btnHitung3 = findViewById(R.id.btnHitung3);
+        btnHasil = findViewById(R.id.btnHasil);
         btnHitung = findViewById(R.id.btnHitung);
         btn_float = findViewById(R.id.btn_float);
         btn_float5 = findViewById(R.id.btn_float5);
@@ -47,7 +48,8 @@ public class Main6aActivity extends AppCompatActivity {
         btnHitung3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final double X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,totalX,Xi ,XiXa;
+                final double X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,totalX,Xi ,XiXa,XiXa2;
+
                 //double Rerata_hujan;
 
                 if (txtX1.getText().toString().isEmpty()){ X1 = 0 ; }else { X1 = Double.parseDouble(txtX1.getText().toString()); }
@@ -58,13 +60,14 @@ public class Main6aActivity extends AppCompatActivity {
                 if (txtX6.getText().toString().isEmpty()){ X6 = 0 ; }else { X6 = Double.parseDouble(txtX6.getText().toString()); }
                 if (txtX7.getText().toString().isEmpty()){ X7 = 0 ; }else { X7 = Double.parseDouble(txtX7.getText().toString()); }
                 if (txtX8.getText().toString().isEmpty()){ X8 = 0 ; }else { X8 = Double.parseDouble(txtX8.getText().toString()); }
-                if (txtX9.getText().toString().isEmpty()){ X9 = 0 ; }else { X9 = Double.parseDouble(txtX8.getText().toString()); }
+                if (txtX9.getText().toString().isEmpty()){ X9 = 0 ; }else { X9 = Double.parseDouble(txtX9.getText().toString()); }
                 if (txtX10.getText().toString().isEmpty()){ X10 = 0 ; }else { X10 = Double.parseDouble(txtX10.getText().toString()); }
                 if (txtXi.getText().toString().isEmpty()){ Xi = 0 ; }else { Xi = Double.parseDouble(txtXi.getText().toString()); }
                 //Rerata_hujan = Double.parseDouble( bave.getString("Rerata_hujan"));
 
                 totalX = (X1 + X2 +X3 + X4 +X5 +X6 + X7 +X8 +X9 +X10 ) /10;
                 XiXa = Xi - totalX;
+                XiXa2 = Math.pow((Xi - totalX),2) ;
 
                 hasilAveX.setText(Double.toString(totalX));
                 hasilAveX1.setText(Double.toString(XiXa));
@@ -80,23 +83,62 @@ public class Main6aActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         double  Ytr, Sn, Yn,Ktr, Sd, Xtr;
+                        final double XiXaX1,XiXaX2,XiXaX3,XiXaX4,XiXaX5,XiXaX6,XiXaX7,XiXaX8,XiXaX9,XiXaX10,totalXiXaX;
                         if (txtYtr.getText().toString().isEmpty()){ Ytr = 0 ; }else { Ytr = Double.parseDouble(txtYtr.getText().toString()); }
                         if (txtSn.getText().toString().isEmpty()){ Sn = 0 ; }else { Sn = Double.parseDouble(txtSn.getText().toString()); }
                         if (txtYn.getText().toString().isEmpty()){ Yn = 0 ; }else { Yn = Double.parseDouble(txtYn.getText().toString()); }
 
+                        XiXaX1 = Math.pow((X1-totalX),2);
+                        XiXaX2 = Math.pow((X2-totalX),2);
+                        XiXaX3 = Math.pow((X3-totalX),2);
+                        XiXaX4 = Math.pow((X4-totalX),2);
+                        XiXaX5 = Math.pow((X5-totalX),2);
+                        XiXaX6 = Math.pow((X6-totalX),2);
+                        XiXaX7 = Math.pow((X7-totalX),2);
+                        XiXaX8 = Math.pow((X8-totalX),2);
+                        XiXaX9 = Math.pow((X9-totalX),2);
+                        XiXaX10 = Math.pow((X10-totalX),2);
+
+                        totalXiXaX = XiXaX1+XiXaX2+XiXaX3+XiXaX4+XiXaX5+XiXaX6+XiXaX7+XiXaX8+XiXaX9+XiXaX10;
                         Ktr = (Ytr - Yn) /Sn;
-                        Sd = Math.sqrt( Math.pow((XiXa / 9),2)  )  ;
-                        Xtr = totalX * (Ktr * Sd);
+                        Sd = Math.sqrt( (totalXiXaX / 9) )  ;
+                        Xtr = totalX + (Ktr * Sd);
 
                         hasilKtr.setText(Double.toString(Ktr));
                         hasilSd.setText(Double.toString(Sd));
                         hasilXtr.setText(Double.toString(Xtr));
 
-
                         final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        final Intent intentb = new Intent(getApplicationContext(), Main6a_t_hasil.class);
                         Bundle c = new Bundle();
-                        c.putString("Rerata_hujan", bave.getString("Rerata_hujan"));
+                        Bundle koin = new Bundle();
+                        //c.putString("Rerata_hujan", bave.getString("Rerata_hujan"));
+                        c.putString("Rerata_hujan", new Double(Xtr).toString());
+                        //koin.putString("Xi", new Double(Xi).toString());
+                        koin.putString("X1", new Double(X1).toString());
+                        koin.putString("X2", new Double(X2).toString());
+                        koin.putString("X3", new Double(X3).toString());
+                        koin.putString("X4", new Double(X4).toString());
+                        koin.putString("X5", new Double(X5).toString());
+                        koin.putString("X6", new Double(X6).toString());
+                        koin.putString("X7", new Double(X7).toString());
+                        koin.putString("X8", new Double(X8).toString());
+                        koin.putString("X9", new Double(X9).toString());
+                        koin.putString("X10", new Double(X10).toString());
+                        koin.putString("XiXa", new Double(XiXa).toString());
+                        koin.putString("XiXa2", new Double(XiXa2).toString());
+                        koin.putString("Ktr", new Double(Ktr).toString());
+                        koin.putString("totalX", new Double(totalX).toString());
+                        koin.putString("Sd", new Double(Sd).toString());
+                        koin.putString("Xtr", new Double(Xtr).toString());
                         intent.putExtras(c);
+                        intentb.putExtras(koin);
+                        btnHasil.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                startActivity(intentb);
+                            }
+                        });
                         btnNext.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
---
+++ app/src/main/java/id/ac/upj/tif/menghitungluas/Main6a_t_hasil.java
@@ -0,0 +1,124 @@
+package id.ac.upj.tif.menghitungluas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main6a_t_hasil extends AppCompatActivity {
+    TextView txtXi1,txtXi2,txtXi3,txtXi4,txtXi5,txtXi6,txtXi7,txtXi8,txtXi9,txtXi10;
+    TextView txtXiXaX1,txtXiXaX2,txtXiXaX3,txtXiXaX4,txtXiXaX5,txtXiXaX6,txtXiXaX7,txtXiXaX8,txtXiXaX9,txtXiXaX10;
+    TextView txtxiXa1,txtxiXa2,txtxiXa3,txtxiXa4,txtxiXa5,txtxiXa6,txtxiXa7,txtxiXa8,txtxiXa9,txtxiXa10;
+    TextView txtKtr2,txtKtr5,txtKtr10,txtKtr25,txtKtr50,txtKtr100,txtKtr200,txtKtr1000;
+    TextView txtXtr2,txtXtr5,txtXtr10,txtXtr25,txtXtr50,txtXtr100,txtXtr200,txtXtr1000;
+    TextView txtxbar, txtSd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6a_t_hasil);
+
+        final Bundle bave = getIntent().getExtras();
+        txtXi1 =  findViewById(R.id.Xi1);
+        txtXi2 =  findViewById(R.id.Xi2);
+        txtXi3 =  findViewById(R.id.Xi3);
+        txtXi4 =  findViewById(R.id.Xi4);
+        txtXi5 =  findViewById(R.id.Xi5);
+        txtXi6 =  findViewById(R.id.Xi6);
+        txtXi7 =  findViewById(R.id.Xi7);
+        txtXi8 =  findViewById(R.id.Xi8);
+        txtXi9 =  findViewById(R.id.Xi9);
+        txtXi10 =  findViewById(R.id.Xi10);
+        txtxiXa1 =  findViewById(R.id.xiXa1);
+        txtxiXa2 =  findViewById(R.id.xiXa2);
+        txtxiXa3 =  findViewById(R.id.xiXa3);
+        txtxiXa4 =  findViewById(R.id.xiXa4);
+        txtxiXa5 =  findViewById(R.id.xiXa5);
+        txtxiXa6 =  findViewById(R.id.xiXa6);
+        txtxiXa7 =  findViewById(R.id.xiXa7);
+        txtxiXa8 =  findViewById(R.id.xiXa8);
+        txtxiXa9 =  findViewById(R.id.xiXa9);
+        txtxiXa10 =  findViewById(R.id.xiXa10);
+        txtXiXaX1 =  findViewById(R.id.XiXaX1);
+        txtXiXaX2 =  findViewById(R.id.XiXaX2);
+        txtXiXaX3 =  findViewById(R.id.XiXaX3);
+        txtXiXaX4 =  findViewById(R.id.XiXaX4);
+        txtXiXaX5 =  findViewById(R.id.XiXaX5);
+        txtXiXaX6 =  findViewById(R.id.XiXaX6);
+        txtXiXaX7 =  findViewById(R.id.XiXaX7);
+        txtXiXaX8 =  findViewById(R.id.XiXaX8);
+        txtXiXaX9 =  findViewById(R.id.XiXaX9);
+        txtXiXaX10 =  findViewById(R.id.XiXaX10);
+
+        txtKtr2 = findViewById(R.id.Ktr2);
+        txtKtr5 = findViewById(R.id.Ktr5);
+        txtKtr10 = findViewById(R.id.Ktr10);
+        txtKtr25 = findViewById(R.id.Ktr25);
+        txtKtr50 = findViewById(R.id.Ktr50);
+        txtKtr100 = findViewById(R.id.Ktr100);
+        txtKtr200 = findViewById(R.id.Ktr200);
+        txtKtr1000 = findViewById(R.id.Ktr1000);
+        txtXtr2 = findViewById(R.id.Xtr2);
+        txtXtr5 = findViewById(R.id.Xtr5);
+        txtXtr10 = findViewById(R.id.Xtr10);
+        txtXtr25 = findViewById(R.id.Xtr25);
+        txtXtr50 = findViewById(R.id.Xtr50);
+        txtXtr100 = findViewById(R.id.Xtr100);
+        txtXtr200 = findViewById(R.id.Xtr200);
+        txtXtr1000 = findViewById(R.id.Xtr1000);
+        txtxbar = findViewById(R.id.xbar);
+        txtSd = findViewById(R.id.Sd);
+
+        txtXi1.setText( bave.getString("X1"));
+        txtXi2.setText( bave.getString("X2"));
+        txtXi3.setText( bave.getString("X3"));
+        txtXi4.setText( bave.getString("X4"));
+        txtXi5.setText( bave.getString("X5"));
+        txtXi6.setText( bave.getString("X6"));
+        txtXi7.setText( bave.getString("X7"));
+        txtXi8.setText( bave.getString("X8"));
+        txtXi9.setText( bave.getString("X9"));
+        txtXi10.setText( bave.getString("X10"));
+
+        txtxiXa1.setText( bave.getString("XiXa"));
+        txtxiXa2.setText( bave.getString("XiXa"));
+        txtxiXa3.setText( bave.getString("XiXa"));
+        txtxiXa4.setText( bave.getString("XiXa"));
+        txtxiXa5.setText( bave.getString("XiXa"));
+        txtxiXa6.setText( bave.getString("XiXa"));
+        txtxiXa7.setText( bave.getString("XiXa"));
+        txtxiXa8.setText( bave.getString("XiXa"));
+        txtxiXa9.setText( bave.getString("XiXa"));
+        txtxiXa10.setText( bave.getString("XiXa"));
+
+        txtXiXaX1.setText( bave.getString("XiXa2"));
+        txtXiXaX2.setText( bave.getString("XiXa2"));
+        txtXiXaX3.setText( bave.getString("XiXa2"));
+        txtXiXaX4.setText( bave.getString("XiXa2"));
+        txtXiXaX5.setText( bave.getString("XiXa2"));
+        txtXiXaX6.setText( bave.getString("XiXa2"));
+        txtXiXaX7.setText( bave.getString("XiXa2"));
+        txtXiXaX8.setText( bave.getString("XiXa2"));
+        txtXiXaX9.setText( bave.getString("XiXa2"));
+        txtXiXaX10.setText( bave.getString("XiXa2"));
+
+        txtKtr2.setText( bave.getString("XiXa2"));
+        txtKtr5.setText( bave.getString("XiXa2"));
+        txtKtr10.setText( bave.getString("XiXa2"));
+        txtKtr25.setText( bave.getString("XiXa2"));
+        txtKtr50.setText( bave.getString("XiXa2"));
+        txtKtr100.setText( bave.getString("XiXa2"));
+        txtKtr200.setText( bave.getString("XiXa2"));
+        txtKtr1000.setText( bave.getString("XiXa2"));
+        txtXtr2.setText( bave.getString("XiXa2"));
+        txtXtr5.setText( bave.getString("XiXa2"));
+        txtXtr10.setText( bave.getString("XiXa2"));
+        txtXtr25.setText( bave.getString("XiXa2"));
+        txtXtr50.setText( bave.getString("XiXa2"));
+        txtXtr100.setText( bave.getString("XiXa2"));
+        txtXtr200.setText( bave.getString("XiXa2"));
+        txtXtr1000.setText( bave.getString("XiXa2"));
+        txtxbar.setText( bave.getString("XiXa2"));
+        txtSd.setText( bave.getString("XiXa2"));
+
+    }
+}
